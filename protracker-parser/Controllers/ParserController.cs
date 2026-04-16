28c29e932283f09ed0d614f1fd86478c0cbe384d
shpvk <holotovegor99@gmail.com>
2026-04-16 20:00:45 +0300

    feat: Add Parser controller logic

---
+++ protracker-parser/Controllers/ParserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using protracker_parser.Models;
+using protracker_parser.Services;
+
+namespace protracker_parser.Controllers
+{
+    [Route("api/[controller]")]
+    public class ParserController : ControllerBase
+    {
+
+        private ParserService ParserService;
+        public ParserController()
+        {
+            ParserService = new ParserService();
+        }
+
+        [HttpPost]
+        public IActionResult Process([FromBody] UserRequest userRequest)
+        {
+            var result = ParserService.Process(userRequest.InputText);
+            return Ok(result);
+        }
+    }
+}
